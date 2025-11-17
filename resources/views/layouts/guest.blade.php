250e0cd1044e54f675407112616536b212fb06c2
Swallowtail <zuhdi352@gmail.com>
2025-11-17 08:27:18 +0700

    minor

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,12 +7,10 @@
 
         <title>{{ config('app.name', 'KOSMO') }}</title>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="text-gray-900 antialiased">
         
         <div class="min-h-screen flex flex-col sm:flex-row">
 
